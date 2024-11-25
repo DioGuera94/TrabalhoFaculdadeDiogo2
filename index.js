fbdbf4ea20e500aea01db27db266ba4a171ce82f
Diogo da Silva Floriano <digodasilvafloriano@gmail.com>
2024-11-25 16:35:53 -0300

    mudança de pastas

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('./pages/public'));
+app.use(express.static('./public'));
 
 const porta = 3000;
 const host = '0.0.0.0';
