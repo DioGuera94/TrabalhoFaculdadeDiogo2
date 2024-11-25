9bbebd2e11e3f0d8eabe97e5e4851ff7b08eca23
Diogo da Silva Floriano <digodasilvafloriano@gmail.com>
2024-11-25 16:30:01 -0300

    Voltando atras!

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running...');
-});
-
-
 import express from 'express';
 
 import session from 'express-session';
 
 import cookieParser from 'cookie-parser';
 
+
+const app = express();
 
 app.use(session({
     secret: 'M1nh4Chav3S3cr3t4',
